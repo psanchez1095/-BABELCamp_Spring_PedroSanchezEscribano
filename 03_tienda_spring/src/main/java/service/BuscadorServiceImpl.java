6b1f54cfc026c30281e913a53f4aabf30c71d9eb
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-04-19 11:44:30 +0200

    Tienda Spring 03 -> Updated

--- 03_tienda_spring/src/main/java/service/BuscadorServiceImpl.java
+++ 03_tienda_spring/src/main/java/service/BuscadorServiceImpl.java
@@ -11,7 +11,7 @@ import model.Producto;
 //Instancia una clase mediante Spring
 @Service
 public class BuscadorServiceImpl implements BuscadorService {
-
+	
 	static ArrayList<Producto> productos = new ArrayList<>(List.of(new Producto("vino", "alimentación", 8.4, 25),
 			new Producto("leche", "alimentación", 1.2, 70), new Producto("cable usb", "informática", 5.6, 30),
 			new Producto("arroz", "alimentación", 3.1, 16), new Producto("silla", "hogar", 30.6, 10),
