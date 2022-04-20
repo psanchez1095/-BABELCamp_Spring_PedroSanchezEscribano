b30fd6faf3ca61ca8ccd097c470291a333db833d
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-04-20 14:04:42 +0200

    Tienda Ajax 06 ->MvcConfig Modificado

--- 06_tienda_ajax/src/main/java/config/MvcConfig.java
+++ 06_tienda_ajax/src/main/java/config/MvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -20,10 +21,20 @@ public class MvcConfig implements WebMvcConfigurer {
 		res.setSuffix(".jsp");
 		return res;
 	}
-
+	
+	//Navegación Estática
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("inicio");
 	}
+	
+	//registro de los recursos estaticos (imagenes, videos, documentos,...)
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/*").addResourceLocations("/");
+		/*registry.addResourceHandler("/*.gif").addResourceLocations("/imagenes");
+		registry.addResourceHandler("/*.css").addResourceLocations("/estilos");*/
+			}
+
 	
 }
